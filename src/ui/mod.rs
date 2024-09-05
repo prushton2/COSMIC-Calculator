96cade06dd6b8d429e850a064cc5f23ab2865660
prushton2 <peterrushton418@gmail.com>
2024-09-05 00:41:56 -0400

    doing styling

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,12 +1,22 @@
 // dont really know what to call this but it contains stuff to prevent circular imports
 
+use cosmic::widget::{button, Button, container};
+use cosmic::iced::widget::{column, row};
+use cosmic::Element;
+
+use cosmic::iced_winit::graphics::core::{Pixels, Length, Padding};
+use cosmic::iced;
+//use cosmic::iced_winit::graphics::core::Vector;
+//use cosmic::iced_winit::graphics::core::border::Radius;
+
 use crate::config::Config;
 
 #[derive(Debug, Clone)]
 pub enum Message {
     ToggleContextPage(ContextPage),
     UpdateConfig(Config),
-    SetPage(Page)
+    SetPage(Page),
+    KeyPressEvent(Key)
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -20,4 +30,61 @@ pub enum Page {
 pub enum ContextPage {
     #[default]
     About,
+}
+
+#[derive(Debug, Clone)]
+pub enum Key {
+    n1,
+    n2,
+    n3,
+    n4,
+    n5,
+    n6,
+    n7,
+    n8,
+    n9,
+    n0
+}
+
+//pub const buttonAppearance: button::Appearance = button::Appearance {
+//    shadow_offset: Vector {x: 0.0, y: 0.0},
+//    background: None,
+//    border_radius: Radius,
+//    border_color: Color,
+//};
+
+//pub const buttonTheme: style::Button = style::Button::Custom {
+//    active:     
+//};
+
+pub fn styled_button<'a>(label: &'a str, key: Key) -> Button<'a, Message> {
+    button(label)
+        .width(Length::Fixed(100.0))
+        .height(Length::Fixed(100.0))
+        .padding(Padding::fit(iced::Size::new(30.0, 30.0), iced::Size::new(90.0, 90.0)))
+        .on_press(Message::KeyPressEvent(key))
+}
+
+pub fn base_calc() -> Element<'static, Message> {
+    let spacing: f32 = 12.0;
+    return container(
+        column![
+            row![
+                styled_button("7", Key::n7),
+                styled_button("8", Key::n8),
+                styled_button("9", Key::n9),
+            ].spacing(Pixels(spacing)),
+            row![
+                styled_button("4", Key::n4),
+                styled_button("5", Key::n5),
+                styled_button("6", Key::n6),
+            ].spacing(Pixels(spacing)),
+            row![
+                styled_button("1", Key::n1),
+                styled_button("2", Key::n2),
+                styled_button("3", Key::n3),
+            ].spacing(Pixels(spacing)),
+        ].spacing(Pixels(spacing))
+        
+    ).into();
 }
